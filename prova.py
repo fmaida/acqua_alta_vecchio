4e2d925b753d43bd5ec599c52630c269a50e6552
Francesco Maida <fran@cesco.it>
2011-03-30 14:30:22 +0200

    Eliminato il file prova.py che per l'appunto era solo di prova e non serviva a nulla

--- prova.py
+++
@@ -1,8 +0,0 @@
-import acquaalta
-import twitter
-
-if (__name__ == "__main__"):
-    objAcqua = acquaalta.AcquaAlta()
-    objAcqua = acquaalta.AcquaAlta()
-    cPrevisione = objAcqua.getPrevisione()
-    print(cPrevisione)
